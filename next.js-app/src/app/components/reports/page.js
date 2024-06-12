842cafc3d290e1c7bf5dc6ba0fb4e679a52f39e6
nyangweso-rodgers <rodgerso65@gmail.com>
2024-06-12 07:47:13 +0300

    add href to report pages

--- next.js-app/src/app/components/reports/page.js
+++ next.js-app/src/app/components/reports/page.js
@@ -9,23 +9,37 @@ const ReportsPage = () => {
       <section className={`${pageStyles.section}`}>
         <div className={`container`}>
           <div className={`row`}>
-            <div className={`col-md-6`}>
+            <div className={`col-md-4`}>
               <div>
                 <Link
-                  href={{ pathname: "/components/reports/participants-survey-report" }}
+                  href={{
+                    pathname: "/components/reports/participants-survey-report",
+                  }}
                   className={`card ${cardStyles.card}`}
                 >
                   Participant's Survey Report
                 </Link>
               </div>
             </div>
-            <div className={`col-md-6`}>
+            <div className={`col-md-4`}>
               <div>
                 <Link
-                  href={{ pathname: "/components/reports/delegates-survey-report" }}
+                  href={{
+                    pathname: "/components/reports/delegates-survey-report",
+                  }}
                   className={`card ${cardStyles.card}`}
                 >
                   Delegates Survey Report
+                </Link>
+              </div>
+            </div>
+            <div className={`col-md-4`}>
+              <div>
+                <Link
+                  href={{ pathname: "/components/reports/customers-report" }}
+                  className={`card ${cardStyles.card}`}
+                >
+                  Customers Report
                 </Link>
               </div>
             </div>
