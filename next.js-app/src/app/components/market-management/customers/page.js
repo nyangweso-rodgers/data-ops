f3b8f515df6db96f348dcd2c40651d16505aac8b
nyangweso-rodgers <rodgerso65@gmail.com>
2024-06-18 19:51:45 +0300

    add first_name and last_name labels and input

--- next.js-app/src/app/components/market-management/customers/page.js
+++ next.js-app/src/app/components/market-management/customers/page.js
@@ -1,4 +1,6 @@
 import pageStyles from "../../../styles/page.module.css";
+import formStyles from "../../../styles/form.module.css";
+import buttonStyles from "../../../styles/buttons.module.css";
 
 const CustomersPage = () => {
   return (
@@ -12,8 +14,52 @@ const CustomersPage = () => {
             </div>
           </div>
           <div className={`row ${pageStyles.row}`}>
-            <div>
-              <div></div>
+            <div className={`col-md-6`}>
+              <div>
+                <label
+                  htmlFor="first_name"
+                  className={`form-label ${formStyles.label}`}
+                >
+                  First Name
+                </label>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  className={`form-control ${formStyles.input}`}
+                  placeholder="Enter your first name"
+                  required
+                ></input>
+              </div>
+            </div>
+            <div className={`col-md-6`}>
+              <div>
+                <label
+                  htmlFor="last_name"
+                  className={`form-label ${formStyles.label}`}
+                >
+                  Last Name
+                </label>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  className={`form-control ${formStyles.input}`}
+                  placeholder="Enter your last name"
+                  required
+                ></input>
+              </div>
+            </div>
+          </div>
+          <div className={`row ${pageStyles.row} `}>
+            <div className={`${pageStyles.displayFlexRowCenter}`}>
+              <button className={`btn ${buttonStyles.button}`} type="submit">
+                Register
+              </button>
             </div>
           </div>
         </div>
