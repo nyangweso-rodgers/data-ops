cace214bef35bd618359ae6ba3edec3e1d2b70e8
nyangweso-rodgers <rodgerso65@gmail.com>
2024-06-19 07:30:57 +0300

    deleted init-replica-set.js file

--- init-replica-set.js
+++
@@ -1,8 +0,0 @@
-rsconf = {
-  _id: "rsmongo",
-  members: [
-    { _id: 0, host: "mongo:27017" },
-    { _id: 1, host: "mongo-secondary1:27018" },
-    { _id: 2, host: "mongo-secondary2:27019" },
-  ],
-};
