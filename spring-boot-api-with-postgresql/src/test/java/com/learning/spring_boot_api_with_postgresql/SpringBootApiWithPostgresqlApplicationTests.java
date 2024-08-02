f51e232ce2e6584279bc8adbcdfa47317e699d67
nyangweso-rodgers <rodgerso65@gmail.com>
2024-08-02 22:42:42 +0300

    setup of spring boot api and postgresql

---
+++ spring-boot-api-with-postgresql/src/test/java/com/learning/spring_boot_api_with_postgresql/SpringBootApiWithPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring_boot_api_with_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiWithPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
