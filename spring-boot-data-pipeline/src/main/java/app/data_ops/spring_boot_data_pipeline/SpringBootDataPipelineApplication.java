792c279c784661ab3f08a479140cd3f83d49b03c
nyangweso-rodgers <rodgerso65@gmail.com>
2024-08-04 22:06:16 +0300

    add Logger and LoggerFactory

--- spring-boot-data-pipeline/src/main/java/app/data_ops/spring_boot_data_pipeline/SpringBootDataPipelineApplication.java
+++ spring-boot-data-pipeline/src/main/java/app/data_ops/spring_boot_data_pipeline/SpringBootDataPipelineApplication.java
@@ -1,13 +1,19 @@
 package app.data_ops.spring_boot_data_pipeline;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDataPipelineApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootDataPipelineApplication.class);
  
 	public static void main(String[] args) { 
 		SpringApplication.run(SpringBootDataPipelineApplication.class, args);
+		logger.info("Spring Boot Data Pipeline Application started successfully.");
 		//System.out.println("This is My First Java Program"); 
 	}
 
