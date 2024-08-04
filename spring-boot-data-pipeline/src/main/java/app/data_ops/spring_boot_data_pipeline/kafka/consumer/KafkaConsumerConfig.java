6c31d5ba3c95f04b229d95dafdacbd03d8519c1b
nyangweso-rodgers <rodgerso65@gmail.com>
2024-08-04 18:19:27 +0300

    add kafka package

---
+++ spring-boot-data-pipeline/src/main/java/app/data_ops/spring_boot_data_pipeline/kafka/consumer/KafkaConsumerConfig.java
@@ -0,0 +1,12 @@
+package app.data_ops.spring_boot_data_pipeline.kafka.consumer;
+
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class KafkaConsumerConfig { 
+    public static void main(String[] args) { 
+		System.out.println("Test Kafka Consumer"); 
+	}  
+}
---
+++ spring-boot-data-pipeline/src/main/java/app/data_ops/spring_boot_data_pipeline/kafka/producer/KafkaProducerConfig.java
@@ -0,0 +1,10 @@
+package app.data_ops.spring_boot_data_pipeline.kafka.producer;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+  public static void main(String[] args) { 
+		System.out.println("Test Kafka Producer"); 
+	}     
+}
