5c0358decc800686df52a43eba11e1695e1493b8
nyangweso-rodgers <rodgerso65@gmail.com>
2024-09-08 15:03:42 +0300

    pytho script to delete a schema

--- apache-kafka/kafka-clients/kafka-python-client/src/app/scripts/02-for-schema-registry/06-delete-schema.py
+++ apache-kafka/kafka-clients/kafka-python-client/src/app/scripts/02-for-schema-registry/06-delete-schema.py
@@ -8,8 +8,8 @@ base_uri = "http://localhost:8081"
 """
     To hard delete a schema, make two `DELETE` requests with the second request setting the permanent parameter to `true` (`/subjects/<subject>/versions/<version>?permanent=true`)
 """
-res = requests.delete(f'{base_uri}/subjects/customers/versions/1').json()
+res = requests.delete(f'{base_uri}/subjects/users.avro.v1customers-value/versions/1').json()
 pretty(res)
 payload = { 'permanent' : 'true' }
-res = requests.delete(f'{base_uri}/subjects/customers/versions/1', params=payload).json()
+res = requests.delete(f'{base_uri}/subjects/users.avro.v1customers-value/versions/1', params=payload).json()
 pretty(res)
