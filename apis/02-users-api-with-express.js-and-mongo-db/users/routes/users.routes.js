ca881d88e2e257052a2a66ac61aa7090d5f84346
nyangweso-rodgers <rodgerso65@gmail.com>
2024-12-18 21:41:54 +0300

    update docker-compose.yml

--- apis/02-users-api-with-express.js-and-mongo-db/users/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import usersController from "../controllers/users.controller.js";
-
-const router = express.Router();
-
-// Define user routes
-router.post("/users", usersController.createUser);
-
-export default router;
