26156ea44574a441b15de4d4896791103eb1e851
nyangweso-rodgers <rodgerso65@gmail.com>
2025-10-11 23:37:59 +0300

    delete jobs/

--- workflows/dagster/dagster_pipeline/jobs/__init__.py
+++
@@ -1,2 +0,0 @@
-# dagster-pipeline/jobs/__init__.py
-#from .mysql_amtdb_accounts_job import mysql_amtdb_accounts_job
--- workflows/dagster/dagster_pipeline/jobs/mysql_amtdb_accounts_job.py
+++
@@ -1,6 +0,0 @@
-from dagster import job
-from dagster_pipeline.assets.etl.mysql_to_clickhouse.amtdb.accounts.v1.accounts_asset import sync_accounts_to_clickhouse
-
-@job
-def mysql_amtdb_accounts_job():
-    sync_accounts_to_clickhouse()
