fcfdbdab2ae81d1580e78b1223d9caf80d52b638
nyangweso-rodgers <rodgerso65@gmail.com>
2025-10-30 21:53:49 +0300

    delete postgres_utils/

--- workflows/dagster/dagster_pipeline/utils/postgres_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# dagster_pipeline/utils/postgres_utils/__init__.py
-from dagster_pipeline.utils.postgres_utils.v1.postgres_utils import PostgresUtils
-
-__all__ = ["PostgresUtils"]
--- workflows/dagster/dagster_pipeline/utils/postgres_utils/v1/__init__.py
+++
@@ -1,2 +0,0 @@
-#from utils.postgres_utils.v1 import PostgreSQLUtils
-# dagster_pipeline/utils/postgres_utils/__init__.py
