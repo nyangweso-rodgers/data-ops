53cd399410a76d03796b9f594b057dfe2ee569bc
nyangweso-rodgers <rodgerso65@gmail.com>
2025-10-30 21:55:19 +0300

    delete clickhouse_utils/

--- workflows/dagster/dagster_pipeline/utils/clickhouse_utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# dagster_pipeline/utils/clickhouse_utils/__init__.py
-from dagster_pipeline.utils.clickhouse_utils.v1.clickhouse_utils import ClickHouseUtils
-__all__ = ["ClickHouseUtils"]
--- workflows/dagster/dagster_pipeline/utils/clickhouse_utils/v1/__init__.py
+++
@@ -1,2 +0,0 @@
-# __init__.py
-#from utils.clickhouse_utils.v1 import ClickHouseUtils
