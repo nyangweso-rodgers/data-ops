34f6f323feb3869d40788cc57bc1d143ec25ef86
nyangweso-rodgers <rodgerso65@gmail.com>
2026-06-20 12:44:44 +0300

    update sql query

--- 01-databases/01-postgres/example_databases/local_dagster/02_tables/kvs/sql/select_kvs.sql
+++ 01-databases/01-postgres/example_databases/local_dagster/02_tables/kvs/sql/select_kvs.sql
@@ -1,5 +1,7 @@
 SELECT id, "key", value
 FROM dagster.kvs
-where key like 'etl_sync_state:mysql:amtdb:%'
+--where key like 'etl_sync_state:mysql:amtdb:%'
 --where key like 'etl_sync_state:mysql:sales-service:%'
+--WHERE key LIKE 'etl_sync_state:clickhouse_export:%'
+--WHERE key LIKE 'etl_sync_state:partner_kaleidofin:%'
 order by key
